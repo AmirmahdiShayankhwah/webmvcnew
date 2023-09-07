501d63794ce73226fc348a4f8065a0e60c80d46d
AmirmahdiShayankhwah <amyrmhdyshaya7@gmail.com>
2023-09-07 23:23:37 +0330

    first web

---
+++ Controllers/Market.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+class Market:Controller
+{
+    public IActionResult Indexb()
+    {
+        return View ();
+    }
+    public IActionResult contact()
+    {
+        return View ();
+    }
+    public IActionResult blog()
+    {
+        return View();
+    }
+    public IActionResult about()
+    {
+        return View();
+    }
+    public IActionResult product()
+    {
+        return View();
+    }
+}
